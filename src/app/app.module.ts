6479d28fe59506f6cc75d80864bfeb70b81ef2d0
mikejpoirier <mike.j.poirier@gmail.com>
2017-05-15 13:06:09 -0400

    Created speech component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SpeechComponent } from './speech/speech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeechComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/speech/speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trv-speech',
+  templateUrl: './speech.component.html',
+  styleUrls: ['./speech.component.scss']
+})
+export class SpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
